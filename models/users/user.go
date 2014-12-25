8e48b0a8197cfbb7cada067e7639c4b3e299aa4e
njdup <njdupp@gmail.com>
2014-12-24 22:58:57 -0500

    Added method to save new user object

--- models/users/user.go
+++ models/users/user.go
@@ -7,8 +7,8 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
-	"../../db/"
 	"github.com/njdup/wakeup-call-backend/conf"
+	"github.com/njdup/wakeup-call-backend/db"
 )
 
 type User struct {
@@ -23,6 +23,58 @@ type User struct {
 var (
 	collectionName = "users"
 )
+
+// Save inserts the receiver User into the database.
+// Returns an error if one is encountered, including
+// validation errors such as a user with the set username
+// already existing
+func (user *User) Save() error {
+    emptyFields := checkEmptyFields(user)
+    if len(emptyFields) != 0 {
+        return errors.New("The following fields cannot be empty:" + emptyFields)
+    }
+
+    insertQuery := func(col *mgo.Collection) error {
+        count, err := col.Find(bson.M{"userName": user.Username}).Limit(1).Count()
+        if err != nil {
+            return err
+        }
+        if count != 0 {
+            return errors.New("A user with the given username already exists")
+        }
+        return col.Insert(user)
+    }
+
+    // TODO: Add insert time stamp here
+
+    err := db.ExecWithCol(collectionName, insertQuery)
+    if err != nil {
+        return errors.New("Error encountered inserting user information into the db")
+    }
+    return nil
+}
+
+
+/*
+ * User model utility functions
+ */
+
+// checkEmptyFields ensures all required fields of a user obj are set
+func checkEmptyFields(user *User) []string {
+    result := make([]string)
+
+    // TODO: Find a better way of doing this kind of checking
+    if user.Username == nil {
+        result = append(result, "username")
+    }
+    return result
+}
+
+
+/*
+ * Testing Code (Bad form, should just make actual tests, but this'll do as I
+ * continue learning Go to begin with ^_^)
+ */
 
 func TestInsert() {
 	testEntry := &User{
