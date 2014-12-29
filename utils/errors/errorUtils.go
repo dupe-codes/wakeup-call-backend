6f3a692a13d7c9369579a4ed3e91064918d39c9b
njdup <njdupp@gmail.com>
2014-12-28 22:11:31 -0500

    Fixed error with errorUtils

--- utils/errors/errorUtils.go
+++ utils/errors/errorUtils.go
@@ -4,9 +4,11 @@ type GeneralError struct {
     Message string
 }
 
+func (err *GeneralError) Error() string { return err.Message }
+
 type InvalidFieldsError struct {
-	GeneralError
+	Message string
 	Fields  []string
 }
 
-func (err *GeneralError) Error() string { return err.Message }
+func (err *InvalidFieldsError) Error() string { return err.Message }
